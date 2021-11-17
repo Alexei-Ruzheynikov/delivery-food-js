d70c0dc948388afaf0c82bc016c06b45b0b6b940
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-11-17 10:54:51 +0300

    Вывод ресторана, вывод товаров ресторана, при клике на лого - возврат на рестораны

--- js/main.js
+++ js/main.js
@@ -18,6 +18,12 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurants = document.querySelector(".cards-restaurants");
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector(".restaurants");
+const menu = document.querySelector(".menu");
+const logo = document.querySelector(".logo");
+const cardsMenu = document.querySelector(".cards-menu");
 
 let login = localStorage.getItem("gloDelivery");
 
@@ -97,3 +103,88 @@ function checkAuth() {
 }
 
 checkAuth();
+
+//day 2
+function createCardRestaurant() {
+  const card = `
+  <a class="card card-restaurant">
+    <img
+      src="img/pizza-plus/preview.jpg"
+      alt="image"
+      class="card-image"
+    />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title">Пицца плюс</h3>
+        <span class="card-tag tag">50 мин</span>
+      </div>
+      <div class="card-info">
+        <div class="rating">4.5</div>
+        <div class="price">От 900 ₽</div>
+        <div class="category">Пицца</div>
+      </div>
+    </div>
+  </a>
+  `;
+  cardsRestaurants.insertAdjacentHTML("beforeend", card);
+}
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
+
+function createCardGood() {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.insertAdjacentHTML(
+    "beforeend",
+    `
+    <img
+      src="img/pizza-plus/pizza-classic.jpg"
+      alt="image"
+      class="card-image"
+    />
+    <div class="card-text">
+      <div class="card-heading">
+        <h3 class="card-title card-title-reg">Пицца Классика</h3>
+      </div>
+      <div class="card-info">
+        <div class="ingredients">
+          Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина,
+          салями, грибы.
+        </div>
+      </div>
+      <div class="card-buttons">
+        <button class="button button-primary button-add-cart">
+          <span class="button-card-text">В корзину</span>
+          <span class="button-cart-svg"></span>
+        </button>
+        <strong class="card-price-bold">510 ₽</strong>
+      </div>
+    </div>
+  `
+  );
+  cardsMenu.insertAdjacentElement("beforeend", card);
+}
+
+function openGoods(event) {
+  const target = event.target;
+  const restaurant = target.closest(".card-restaurant");
+  if (restaurant) {
+    cardsMenu.textContent = "";
+    containerPromo.classList.add("hide");
+    restaurants.classList.add("hide");
+    menu.classList.remove("hide");
+
+    createCardGood();
+    createCardGood();
+    createCardGood();
+  }
+}
+
+cardsRestaurants.addEventListener("click", openGoods);
+logo.addEventListener("click", function () {
+  containerPromo.classList.remove("hide");
+  restaurants.classList.remove("hide");
+  menu.classList.add("hide");
+});
